1f3e062e1bdde8a82c971550c4b8264ee05a6d2c
Rcardo-M <404232992@qq.com>
2020-03-08 14:51:28 +0800

    先不写混沌策略了

--- FA-base/chaosAlgorithm.py
+++ FA-base/chaosAlgorithm.py
@@ -6,10 +6,17 @@ if __name__ == '__main__':
 
 
 
+    x = np.random.random([1,2])*100
+    print(x)
 
+    for i in range(100):
+        # print(np.random.random([1,2])*100)
+        x = np.append(x,np.random.random([1,2])*100,axis=0)
+
+    print(x)
     plt.axis([-100, 100, -100, 100])
 
-    plt.plot([1, 2, 3, 4], [1, 4, 9, 16], 'ro')
+    plt.scatter(x[:,0],x[:,1],c='r')
     plt.show()
     # plt.figure()
 
