b5d4bb6f3509cc71db196c467e19e2a915448103
Rcardo-M <404232992@qq.com>
2020-03-08 16:00:36 +0800

    开始测试  plot

---
+++ FA/plot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+def plot(X_origin, X):
+    fig_origin = plt.figure(0)
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.scatter(X_origin[:, 0], X_origin[:, 1], c='r')
+    plt.scatter(X[:, 0], X[:, 1], c='g')
+    plt.pause(0.1)
+    plt.clf()
