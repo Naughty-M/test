fc0048a53e64d9d6869979cdf24a4dce4d455071
Rcardo-M <404232992@qq.com>
2020-03-22 23:01:34 +0800

    添鲸鱼算法

---
+++ AFS/run.py
@@ -0,0 +1,7 @@
+import numpy as np
+import AFS
+
+if __name__ == "__main__":
+    bound = np.tile([[-600], [600]], 25)
+    afs = AFS(60, 25, bound, 500, [0.001, 0.0001, 0.618, 40])
+    afs.solve()
